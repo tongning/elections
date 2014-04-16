fc31fae87329e10f1a1a10f47bca4b9b3e0ed357
Anthony <tongning05@gmail.com>
2014-04-16 13:59:27 -0400

    Add vote tallying features

---
+++ public/admcount.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'db.inc.php';
+include_once 'functions.inc.php';
+$db=new PDO(DB_INFO,DB_USER,DB_PASS);
+session_start();
+if(isset($_SESSION['is_admin']) && $_SESSION['is_admin']==1){
+	echo "<h1>Current Vote Tally</h1>";
+	$sql="SELECT * FROM candidates";
+	$stmt=$db->prepare($sql);
+	$stmt->execute();
+	while($currcandidate = $stmt->fetch()){
+		echo $currcandidate['firstname']." ".$currcandidate['lastname'].": ";
+		$sql = "SELECT COUNT(*) AS num_votes FROM votes WHERE candidateid=?";
+		$stm = $db->prepare($sql);
+		$stm->execute(array($currcandidate['candidateid']));
+		$response = $stm->fetch();
+		echo $response['num_votes']."<br>";
+	}
+	
+
+}
+else{
+	header('Location:/admlogin.php');
+}
+?>
--- public/admhome.php
+++ public/admhome.php
@@ -2,6 +2,7 @@
 session_start();
 if(isset($_SESSION['is_admin']) && $_SESSION['is_admin']==1){
 	echo "<h1>Administrator Home</h1><h3>Tasks</h3>
+	<a href='admcount.php'>Tally Votes</a><br>
 	<a href='admcodes.php'>Generate Voter Codes</a><br>
 	<a href='admlogout.php'>Log out</a>
 	
