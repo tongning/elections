35963e437983a07be3d5983794a08552fa689efd
tongning <tongning05@gmail.com>
2014-04-16 13:02:02 -0400

    Add testing index

---
+++ public/althome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+<title>SGA Election Login</title>
+</head>
+<body>
+<p>Enter BILL username and password to log in</p>
+<form name="userlogin" action="voterlogin.php" method="POST">
+BILL/BINX Username - (Will be changed to Student ID later):<br><input name="id" type="text"><br>
+School password:<br><input name="schoolpass" type="password"></br>
+<input type="submit" value="Begin voting">
+</form>
+</body>
+</html>
