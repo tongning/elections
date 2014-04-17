d0caec4d277fc3df506571108aa0ba57c184d7cb
Anthony <tongning05@gmail.com>
2014-04-17 17:04:55 -0400

    add comment

---
+++ public/db.inc.php
@@ -0,0 +1,7 @@
+
+<?php
+//modify password before election
+define('DB_INFO','mysql:host=localhost;dbname=sga_elections');
+define('DB_USER','sga_elections');
+define('DB_PASS','G5XL6MhveTjuRWMs');
+?>
